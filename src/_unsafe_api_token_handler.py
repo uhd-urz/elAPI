85158d480a990b5f2d5a33c66bda226981ac60ca
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-07-29 19:44:53 +0200

    Add warning method for unsafe API token use

---
+++ src/_unsafe_api_token_handler.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from dynaconf.utils import inspect
+from src.loggers import logger
+from dynaconf import Dynaconf
+
+
+def inspect_api_token_location(setting_object: Dynaconf, unsafe_path: Path):
+    history = inspect.get_history(setting_object)
+    for d in history:
+        if Path(d["identifier"]).absolute() == unsafe_path.absolute():
+            if "API_TOKEN" in d["value"]:
+                logger.warning(
+                    f"api_token in project-based configuration file found. This is highly discouraged. The api_token "
+                    f"is at risk of being leaked into public repositories. If you still insist, please make sure "
+                    f"elabftw-get.yaml is included in .gitignore.")
