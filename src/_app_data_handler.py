8be87f15259506414185456cdefc5d9cb94185c9
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-08 22:02:01 +0200

    Add option that creates parent directories if they don't exist

--- src/_app_data_handler.py
+++ src/_app_data_handler.py
@@ -6,14 +6,14 @@ APP_DATA_PARENT_DIR_NAME = "elabftw-get"
 # Reference: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 # App unit_data
 LOCAL_DATA_DIR = os.getenv('XDG_DATA_HOME')
-ASSUMED_LOCAL_DATA_DIR = os.getenv('HOME') + '/.local/share'  # based on popular convention
+ASSUMED_LOCAL_DATA_DIR = os.getenv('HOME') + '/.local/share'  # if XDG_DATA_HOME isn't defined in the machine
 APP_DATA_DIR = Path(LOCAL_DATA_DIR) / APP_DATA_PARENT_DIR_NAME if LOCAL_DATA_DIR else Path(
     ASSUMED_LOCAL_DATA_DIR) / APP_DATA_PARENT_DIR_NAME
-APP_DATA_DIR.mkdir(exist_ok=True)
+APP_DATA_DIR.mkdir(parents=True, exist_ok=True)  # parents=True => mkdir -p
 
 # App log unit_data
 LOG_DIR = APP_DATA_DIR
 
 # API response unit_data
 RESPONSE_DATA_DIR = Path('/var/tmp/elabftw-get')
-RESPONSE_DATA_DIR.mkdir(exist_ok=True)
+RESPONSE_DATA_DIR.mkdir(parents=True, exist_ok=True)
