d0a6050c13d2a4afc442cd28b24cfdda4475362f
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-22 23:57:35 +0200

    Add InspectConfig()
    Generalize Dynaconf inspection of settings.
    - Add attribute that stores configuration identifiers (files)
    - Add attribute that stores active configuration fields
    - Allow active configuration to be stored in APP_DATA_DIR for debugging

--- src/_unsafe_api_token_handler.py
+++ src/_unsafe_api_token_handler.py
@@ -1,16 +1,64 @@
+import json
+from dataclasses import dataclass
 from pathlib import Path
-from dynaconf.utils import inspect
-from src.loggers import logger
+
 from dynaconf import Dynaconf
-from src.core_names import CONFIG_FILE_NAME
+from dynaconf.utils import inspect
+
+from src._path_handler import ProperPath
+from src.core_names import CONFIG_FILE_NAME, APP_DATA_DIR, CONFIG_HISTORY_FILE_NAME
+from src.loggers import logger
 
 
-def inspect_api_token_location(setting_object: Dynaconf, unsafe_path: Path):
-    history = inspect.get_history(setting_object)
-    for d in history:
-        if Path(d["identifier"]).absolute() == unsafe_path.absolute():
-            if "API_TOKEN" in d["value"]:
-                logger.warning(
-                    f"api_token in project-based configuration file found. This is highly discouraged. The api_token "
-                    f"is at risk of being leaked into public repositories. If you still insist, please make sure "
-                    f"{CONFIG_FILE_NAME} is included in .gitignore.")
+@dataclass
+class InspectConfig:
+    setting_object: Dynaconf
+    config_history_location: Path = APP_DATA_DIR / CONFIG_HISTORY_FILE_NAME
+
+    @property
+    def history(self):
+        return inspect.get_history(self.setting_object)
+
+    @history.setter
+    def history(self, value):
+        raise AttributeError("Configuration history isn't meant to modified.")
+
+    @property
+    def inspect_applied_config_files(self):
+        applied_config_files = set()
+        for config in self.history:
+            applied_config_files.add(config["identifier"])
+        return applied_config_files
+
+    @inspect_applied_config_files.setter
+    def inspect_applied_config_files(self, value):
+        raise AttributeError("Configuration history isn't meant to modified.")
+
+    @property
+    def inspect_applied_config(self):
+        applied_config = {}
+        for config in self.history:
+            for k, v in config["value"].items():
+                config["value"][k] = v, config["identifier"]
+                applied_config.update(config["value"])
+        applied_config["API_TOKEN"] = "***"
+        return applied_config
+
+    @inspect_applied_config.setter
+    def inspect_applied_config(self, value):
+        raise AttributeError("Configuration history isn't meant to modified.")
+
+    def inspect_api_token_location(self, unsafe_path: Path):
+        for d in self.history:
+            if Path(d["identifier"]).absolute() == unsafe_path.absolute():
+                if "API_TOKEN" in d["value"]:
+                    logger.warning(
+                        f"api_token in project-based configuration file found. This is highly discouraged. "
+                        f"The api_token is at risk of being leaked into public repositories. If you still insist, "
+                        f"please make sure {CONFIG_FILE_NAME} is included in .gitignore.")
+
+    def store(self):
+        store_location = ProperPath(self.config_history_location).create()
+        with store_location.open(mode="w", encoding="utf-8") as file:
+            json.dump(self.inspect_applied_config, file)
+        # print(f"Active configuration history is saved in {self.config_history_location}")
