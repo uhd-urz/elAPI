e9924c301c50fd6bd6a531bb182583f9a8f167fd
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-22 19:23:07 +0200

    Improve path validation for log directories

--- src/_log_file_handler.py
+++ src/_log_file_handler.py
@@ -1,35 +1,22 @@
-import sys
 from datetime import datetime
 from pathlib import Path
 from typing import Union
 
-from src.core_names import FALLBACK_DIR, APP_DATA_DIR, LOG_DIR_ROOT, LOG_FILE_NAME
+from src._path_handler import ProperPath
+from src.core_names import APP_DATA_DIR, LOG_DIR_ROOT, LOG_FILE_NAME
 
-# loggers.py gets its path for storing log from _log_file_handler.py
-# Therefore _log_file_handler.py cannot depend on ProperPath in _path_handler.py
+initial_validation: dict[Path:Union[Path, None]] = {}
+_DIRS: tuple[Path, ...] = LOG_DIR_ROOT, APP_DATA_DIR
 
+for loc in _DIRS:
+    initial_validation[loc] = ProperPath(loc, suppress_stderr=True).resolve()
 
-LOG_DIR: Union[Path, str] = ""
-_DIRS: tuple[Path, ...] = APP_DATA_DIR, LOG_DIR_ROOT, FALLBACK_DIR
-
-for _, loc in enumerate(_DIRS):
-    if loc:
-        try:
-            loc.mkdir(parents=True, exist_ok=True)
-            (loc / LOG_FILE_NAME).touch(exist_ok=True)
-        except PermissionError:
-            print(f"{datetime.now().isoformat(sep=' ', timespec='seconds')}:WARNING: Permission to write logs in "
-                  f"'{loc}/{LOG_FILE_NAME}' has been denied.", file=sys.stderr)
-            print(f"Falling back to write logs in '{_DIRS[_ + 1]}/{LOG_FILE_NAME}'.", file=sys.stderr) \
-                if _ + 1 < len(_DIRS) else ...
-        else:
-            LOG_DIR = loc
-            break
+LOG_DIR, *_ = [loc for loc in initial_validation.values() if loc is not None]
 
 if not LOG_DIR:
     raise PermissionError(
-        f"{datetime.now().isoformat(sep=' ', timespec='seconds')}: FATAL: Permission to write logs in fallback path "
-        f"{FALLBACK_DIR}/{LOG_FILE_NAME} has been denied as well! This is a critical error.\n"
+        f"{datetime.now().isoformat(sep=' ', timespec='seconds')}:FATAL: Permission to write logs in fallback path "
+        f"{APP_DATA_DIR}/{LOG_FILE_NAME} is denied as well! This is a critical error.\n"
         f"elabftw-get will not run!")
 
 LOG_FILE_PATH: Path = LOG_DIR / LOG_FILE_NAME
