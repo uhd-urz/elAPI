30c69a06548c549862c93bff889c4afeac4630a9
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-23 02:15:45 +0200

    Add more imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
-from src._config_handler import records, HOST, API_TOKEN, TOKEN_BEARER, DOWNLOAD_DIR, TMP_DIR
+from src._config_handler import records, HOST, API_TOKEN, TOKEN_BEARER, DOWNLOAD_DIR, APP_DATA_DIR, TMP_DIR
 from src._log_file_handler import LOG_FILE_PATH
+from src.core_names import APP_NAME
