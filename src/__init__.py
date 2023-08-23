46254ba76fe5341faff9841f80a5fc66b4fb9b4c
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-24 00:35:18 +0200

    import ProperPath

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from src._config_handler import records, HOST, API_TOKEN, TOKEN_BEARER, DOWNLOAD_DIR, APP_DATA_DIR, TMP_DIR
 from src._log_file_handler import LOG_FILE_PATH
+from src._path_handler import ProperPath
 from src.core_names import APP_NAME
