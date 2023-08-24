1a9f52baf6454cf5ae171cb08e73d903ca55450e
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-24 23:27:35 +0200

    Add documentation for option 'output'

--- cli/_doc.py
+++ cli/_doc.py
@@ -3,11 +3,15 @@ This script includes docstring for elabftw-get. The docstrings are mainly meant 
 """
 
 __PARAMETERS__doc__ = {
-    "endpoint": f"Name of an endpoint. Valid endpoints are: apikeys, config, experiments, "
-                f"items, experiments_templates, items_types, event, events, team_tags, teams, "
-                f"todolist, unfinished_steps, users.",
-    "unit_id": f"ID for one of the preceding endpoints. If provided, only information associated with that "
-               f"ID will be returned. E.g., user ID, team ID, experiments ID.",
-    "plaintext": f"When --plaintext is provided, response will be sent to STDOUT un-formatted (without syntax "
-                 f"highlighting).This can be utilized if one wishes to use external formatting."
+    "endpoint": "Name of an endpoint. Valid endpoints are: apikeys, config, experiments, "
+                "items, experiments_templates, items_types, event, events, team_tags, teams, "
+                "todolist, unfinished_steps, users.",
+    "unit_id": "ID for one of the preceding endpoints. If provided, only information associated with that "
+               "ID will be returned. E.g., user ID, team ID, experiments ID.",
+    "output": "Format style for printing output. Supported values are: **JSON**, **YAML**, **plaintext**. "
+              "The values are case insensitive. The default format is `JSON`. "
+              "When 'plaintext' is used, response will be sent to STDOUT in *original*, un-formatted, "
+              "without syntax highlighting. This can be utilized if one wishes to pipe the output to "
+              "some external formatting program like `less`. "
+              "If an unsupported format value is provided then the output falls back to '-plaintext'."
 }
