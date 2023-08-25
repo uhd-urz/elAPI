f026a53e1b8eb745dc0f7fb5826dc881e138ce77
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-25 22:59:37 +0200

    Update module name after change

--- cli/elabftw_get.py
+++ cli/elabftw_get.py
@@ -22,7 +22,7 @@ from rich.console import Console
 from rich.markdown import Markdown
 from typing_extensions import Annotated
 
-from apps.debug_info import info
+from apps.show_config import info
 from cli._doc import __PARAMETERS__doc__ as docs
 from cli._highlight_syntax import Highlight
 from cli._markdown_doc import _get_custom_help_text
