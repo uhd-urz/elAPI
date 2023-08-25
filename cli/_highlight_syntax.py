ad16ecf83839c8673ba586ac0c5d6fb1040e62f2
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-25 21:33:30 +0200

    Improve regex matching of output format

--- cli/_highlight_syntax.py
+++ cli/_highlight_syntax.py
@@ -13,10 +13,24 @@ from src import logger
 
 @dataclass
 class Highlight:
-    SUPPORTED_FORMAT: ClassVar[dict] = {"JSON": r"json", "YAML": r"ya?ml", "text": r"(plain)?text"}
+    SUPPORTED_FORMAT: ClassVar[dict] = {
+        "JSON": {
+            "pattern": r"json",
+            "parser": lambda data: json.dumps(data, indent=True, ensure_ascii=True)
+        },
+        "YAML": {
+            "pattern": r"ya?ml",
+            "parser": lambda data: yaml.dump(data)
+        },
+        "TEXT": {
+            "pattern": r"(plain)?text",
+            "parser": lambda data: data
+        }
+    }
+    FALLBACK_FORMAT: ClassVar[str] = "TEXT"
     console: ClassVar[Console] = Console(color_system="truecolor")
     data: Union[dict, str]
-    lang: str = "json"
+    lang: str = "JSON"
     theme: str = "lightbulb"
 
     def __post_init__(self):
@@ -28,27 +42,23 @@ class Highlight:
 
     @language.setter
     def language(self, value):
-        if not re.match(r"|".join(Highlight.SUPPORTED_FORMAT.values()), value, flags=re.IGNORECASE):
+        for key in Highlight.SUPPORTED_FORMAT:
+            if re.match(Highlight.SUPPORTED_FORMAT[key]["pattern"], value, flags=re.IGNORECASE):
+                self.lang = key
+                break
+
+        if self.lang not in Highlight.SUPPORTED_FORMAT:
             logger.error(f"The provided format '{value}' cannot be highlighted! "
-                         f"Supported formats are: JSON, YAML, plaintext.")
+                         f"Supported formats are: {Highlight.SUPPORTED_FORMAT.keys()}")
             print("\n", file=sys.stderr)
-            self.lang = "text"  # falls back to "text"
-        else:
-            self.lang = value
+            self.lang = Highlight.FALLBACK_FORMAT  # falls back to "text"
 
     def get_syntax(self, data):
         return Syntax(data, self.language, background_color="default", theme=self.theme)
 
     @property
     def format(self):
-        if re.match(Highlight.SUPPORTED_FORMAT["JSON"], self.language, flags=re.IGNORECASE):
-            return json.dumps(self.data, indent=True, ensure_ascii=True)
-
-        elif re.match(Highlight.SUPPORTED_FORMAT["YAML"], self.language, flags=re.IGNORECASE):
-            return yaml.dump(self.data)
-
-        else:
-            return self.data
+        return Highlight.SUPPORTED_FORMAT[self.language]["parser"](self.data)
 
     @format.setter
     def format(self, value):
@@ -56,8 +66,8 @@ class Highlight:
 
     def highlight(self):
         formatted = self.format
-        if re.match(Highlight.SUPPORTED_FORMAT["text"], self.language, flags=re.IGNORECASE):
-            print(self.data)
+        if self.language == Highlight.FALLBACK_FORMAT:
+            print(formatted)
         else:
             highlighted = self.get_syntax(data=formatted)
             Highlight.console.print(highlighted)
