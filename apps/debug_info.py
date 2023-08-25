af3c17c17f90d9cb2a223ac642081ed6d2838ee7
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-25 22:56:52 +0200

    Add LEVEL information to source

--- apps/debug_info.py
+++ apps/debug_info.py
@@ -1,11 +1,18 @@
 from src import LOG_FILE_PATH, APP_NAME, HOST, records, TOKEN_BEARER, DOWNLOAD_DIR, APP_DATA_DIR, TMP_DIR
 
+detected_config_files = records.inspect_applied_config_files
+
 api_token_masked, api_token_source = records.inspect_applied_config["API_TOKEN_MASKED"]
+api_token_source = detected_config_files[api_token_source]
+
 unsafe_token_use_value, unsafe_token_use_source = records.inspect_applied_config["UNSAFE_API_TOKEN_WARNING"]
 unsafe_token_use_value = "Yes" if records.inspect_applied_config["API_TOKEN"] else "No"
-host_source = records.inspect_applied_config["HOST"][1]
-download_dir_source = records.inspect_applied_config["DOWNLOAD_DIR"][1]
-detected_config_files = "\n- " + "\n- ".join(records.inspect_applied_config_files)
+unsafe_token_use_source = detected_config_files[unsafe_token_use_source]
+
+host_source = detected_config_files[records.inspect_applied_config["HOST"][1]]
+download_dir_source = detected_config_files[records.inspect_applied_config["DOWNLOAD_DIR"][1]]
+
+detected_config_files_formatted = "\n- " + "\n- ".join(f"`{v}`: {k}" for k, v in detected_config_files.items())
 
 info = f"""
 ## {APP_NAME} configuration information
@@ -13,15 +20,15 @@ The following debug information includes configuration values and their sources 
 > Name: Value ← Source
 
 - **Log file path:** {LOG_FILE_PATH}
-- **Host address:** {HOST} ← {host_source}
-- **API token:** {api_token_masked} ← {api_token_source}
-- **Token bearer:** "{TOKEN_BEARER}"
-- **Download directory:** {DOWNLOAD_DIR} ← {download_dir_source}
+- **Host address:** {HOST} ← `{host_source}`
+- **API token:** {api_token_masked} ← `{api_token_source}`
+- **Token bearer:** {TOKEN_BEARER}
+- **Download directory:** {DOWNLOAD_DIR} ← `{download_dir_source}`
 - **App data directory:** {APP_DATA_DIR}
 - **Caching directory:** {TMP_DIR}
-- **Unsafe API token use warning:** {unsafe_token_use_value} ← {unsafe_token_use_source}
+- **Unsafe API token use warning:** {unsafe_token_use_value} ← `{unsafe_token_use_source}`
 
 
 **_Detected configuration files that are in use:_**
-{detected_config_files}
+{detected_config_files_formatted}
 """
