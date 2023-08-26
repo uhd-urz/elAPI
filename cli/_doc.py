05d401dbf3991078cc52a32e04fa813f8c16bb26
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-08-26 22:13:30 +0200

    Make supported formats dynamic

--- cli/_doc.py
+++ cli/_doc.py
@@ -1,6 +1,9 @@
 """
 This script includes docstring for elabftw-get. The docstrings are mainly meant to be used with a CLI interface.
 """
+from cli._highlight_syntax import Highlight
+
+supported_highlighting_formats = ", ".join(f"**{_}**" for _ in Highlight.SUPPORTED_FORMAT.keys())
 
 __PARAMETERS__doc__ = {
     "endpoint": "Name of an endpoint. Valid endpoints are: apikeys, config, experiments, "
@@ -8,7 +11,7 @@ __PARAMETERS__doc__ = {
                 "todolist, unfinished_steps, users.",
     "unit_id": "ID for one of the preceding endpoints. If provided, only information associated with that "
                "ID will be returned. E.g., user ID, team ID, experiments ID.",
-    "output": "Format style for printing output. Supported values are: **JSON**, **YAML**, **plaintext**. "
+    "output": f"Format style for printing output. Supported values are: {supported_highlighting_formats}. "
               "The values are case insensitive. The default format is `JSON`. "
               "When 'plaintext' is used, response will be sent to STDOUT in *original*, un-formatted, "
               "without syntax highlighting. This can be utilized if one wishes to pipe the output to "
