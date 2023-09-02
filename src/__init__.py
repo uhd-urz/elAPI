a2dbd469aec24a199fba2864eeb5a9db61b727be
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-09-02 16:52:39 +0200

    Add elabftw_throw() method

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from src._api import elabftw_fetch
+from src._api import elabftw_fetch, elabftw_throw
 from src._config_handler import records, HOST, API_TOKEN, TOKEN_BEARER, UNSAFE_TOKEN_WARNING, DOWNLOAD_DIR, APP_DATA_DIR, TMP_DIR
 from src._log_file_handler import LOG_FILE_PATH
 from src._path_handler import ProperPath
