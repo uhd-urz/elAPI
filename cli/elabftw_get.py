aea7d1cef359e8dcf18062a37bf45dcce146a986
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-09-03 13:10:08 +0200

    Add method for making POST requests

--- cli/elabftw_get.py
+++ cli/elabftw_get.py
@@ -12,6 +12,7 @@ The script treats API endpoints as its arguments.
     With elabftw-get you can do the following:
         $ elabftw-get fetch users <id>
 """
+import ast
 
 import typer
 from rich import pretty
@@ -23,7 +24,7 @@ from apps.show_config import info
 from cli._doc import __PARAMETERS__doc__ as docs
 from cli._highlight_syntax import Highlight
 from cli._markdown_doc import _get_custom_help_text
-from src import ProperPath, TMP_DIR, elabftw_fetch
+from src import ProperPath, TMP_DIR, elabftw_fetch, elabftw_post
 
 pretty.install()
 console = Console(color_system="truecolor")
@@ -33,14 +34,17 @@ typer.rich_utils.STYLE_HELPTEXT = ""  # fixes https://github.com/tiangolo/typer/
 typer.rich_utils._get_help_text = _get_custom_help_text  # fixes https://github.com/tiangolo/typer/issues/447
 
 
-@app.command()
-def fetch(endpoint: Annotated[str, typer.Argument(
-    help=docs["endpoint"], show_default=False)],
-          unit_id: Annotated[int, typer.Argument(help=docs["unit_id"])] = None,
-          output: Annotated[str, typer.Option("--output", "-o",
-                                              help=docs["output"], show_default=False)] = "json") -> None:
+@app.command(short_help="Make `GET` requests to elabftw endpoints.")
+def fetch(
+        endpoint: Annotated[str, typer.Argument(
+            help=docs["endpoint"], show_default=False)],
+        unit_id: Annotated[int, typer.Option("--id", "-i", help=docs["unit_id_get"], show_default=False)] = None,
+        output: Annotated[str, typer.Option("--output", "-o",
+                                            help=docs["output"], show_default=False)]
+        = "json"
+) -> None:
     """
-    Make HTTP API requests to elabftw's endpoints as documented in
+    Make `GET` requests to elabftw endpoints as documented in
     [https://doc.elabftw.net/api/v2/](https://doc.elabftw.net/api/v2/).
 
     <br/>
@@ -50,12 +54,66 @@ def fetch(endpoint: Annotated[str, typer.Argument(
     > GET /users/{id}
 
     With `elabftw-get` you can do the following:
-    `$ elabftw-get fetch users <id>`
+    <br/>
+    `$ elabftw-get fetch users` will return list of all users.
+    <br/>
+    `$ elabftw-get fetch users --id <id>` will return information about the specific user `<id>`.
     """
 
     raw_response = elabftw_fetch(endpoint=endpoint, unit_id=unit_id)
     prettify = Highlight(data=raw_response.json(), lang=output)
     prettify.highlight()
+
+
+@app.command(
+    short_help="Make `POST` request to elabftw endpoints.",
+    context_settings={
+        "allow_extra_args": True,
+        "ignore_unknown_options": True
+    }
+)
+def post(
+        endpoint: Annotated[str, typer.Argument(
+            help=docs["endpoint"], show_default=False)], *,
+        unit_id: Annotated[int, typer.Option("--id", "-i", help=docs["unit_id_post"], show_default=False)] = None,
+        json_: Annotated[str, typer.Option("--data", "-d", help=docs["data"], show_default=False)] = None,
+        data: typer.Context = None,
+        output: Annotated[str, typer.Option("--output", "-o",
+                                            help=docs["output"], show_default=False)]
+        = "json"
+) -> None:
+    """
+    Make `POST` request to elabftw endpoints as documented in
+    [https://doc.elabftw.net/api/v2/](https://doc.elabftw.net/api/v2/).
+
+    <br/>
+    **Example**:
+    <br/>
+    From [the official documentation about `POST users`](https://doc.elabftw.net/api/v2/#/Users/post-user),
+    > POST /users/{id}
+
+    With `elabftw-get` you can do the following:
+    <br/>
+    `$ elabftw-get post users --firstname John --lastname Doe --email "john_doe@email.com"` will create a new user.
+    """
+    from json import JSONDecodeError
+    if json_:
+        valid_data: dict = ast.literal_eval(json_)
+    else:
+        data_keys: list[str, ...] = [_.removeprefix("--") for _ in data.args[::2]]
+        data_values: list[str, ...] = data.args[1::2]
+        valid_data: dict[str: str, ...] = dict(zip(data_keys, data_values))
+    raw_response = elabftw_post(endpoint, unit_id=unit_id, **valid_data)
+    try:
+        prettify = Highlight(data=raw_response.json(), lang=output)
+    except JSONDecodeError:
+        if raw_response.is_success:
+            console.print("Success: Resource created!", style="green")
+        else:
+            console.print(f"Warning: Something unexpected happened! "
+                          f"The HTTP return was '{raw_response}'.", style="red")
+    else:
+        prettify.highlight()
 
 
 @app.command(name="show-config")
@@ -79,7 +137,7 @@ def cleanup() -> None:
 @app.command(name="bill-teams")
 def bill_teams(output: Annotated[str, typer.Option("--output", "-o",
                                                    help=docs["output"], show_default=False)] = "json") -> None:
-    """*Experimental:* Generate billable teams data."""
+    """*Beta:* Generate billable teams data."""
 
     from apps.bill_teams import get_team_owners
     billable = get_team_owners()
