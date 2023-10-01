72aceffa2e2b0fe54fe1481c48624595a3a7b1c8
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-01 17:29:58 +0200

    Update imports
    Remove imports from __init__ that could cause circular dependency error. In fact those imports were unnecessary to begin with.

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,2 @@
-from src._api import GETRequest, POSTRequest, AsyncGETRequest
-from src._config_handler import (records, HOST, API_TOKEN, TOKEN_BEARER, UNSAFE_TOKEN_WARNING, DOWNLOAD_DIR,
-                                 APP_DATA_DIR, TMP_DIR, CLEANUP_AFTER)
 from src._log_file_handler import LOG_FILE_PATH
-from src._path_handler import ProperPath
-from src._validator import Validate, ConfigValidator, PermissionValidator
-from src.core_names import APP_NAME
-from src.loggers import logger
+from src._names import APP_NAME
