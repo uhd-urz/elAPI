9ef09535c39f436e84ee04cdc752c29495497db6
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-01 17:28:53 +0200

    Use new Logger instance
    - Update import paths
    - Pass new logger instance to ProperPath's "err_logger"

--- src/_log_file_handler.py
+++ src/_log_file_handler.py
@@ -1,23 +1,24 @@
-from datetime import datetime
 from pathlib import Path
 
-from src._path_handler import ProperPath
-from src.core_names import APP_DATA_DIR, LOG_DIR_ROOT, LOG_FILE_NAME, APP_NAME
+from src._names import APP_DATA_DIR, LOG_DIR_ROOT, LOG_FILE_NAME, APP_NAME
+from src.loggers import Logger
+from src.path import ProperPath
+
+logger = Logger()
 
 initial_validation: dict[Path:Path] = {}
 _DIRS: tuple[Path, ...] = LOG_DIR_ROOT, APP_DATA_DIR
 
 for loc in _DIRS:
-    if working_path := ProperPath(loc / LOG_FILE_NAME, suppress_stderr=True).create():
+    if working_path := ProperPath(loc / LOG_FILE_NAME, err_logger=Logger(suppress_stderr=True)).create():
         initial_validation[loc] = working_path
         break
 
 try:
     LOG_FILE_PATH, *_ = initial_validation.values()
 except ValueError as e:
-    raise SystemExit(
-        f"{datetime.now().isoformat(sep=' ', timespec='seconds')}:FATAL: Permission to write logs in fallback path "
-        f"{APP_DATA_DIR}/{LOG_FILE_NAME} is denied as well! This is a critical error.\n"
-        f"{APP_NAME} will not run!") from e
+    logger.critical(f'Permission to write logs in fallback path {APP_DATA_DIR}/{LOG_FILE_NAME} is denied as well! '
+                    f'This is a critical error. {APP_NAME} will not run!"')
+    raise SystemExit() from e
 
 LOG_FILE_PATH: Path = LOG_FILE_PATH  # same variable but type annotated
