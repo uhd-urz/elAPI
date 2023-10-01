e479d09fb37dd2a65a9500aa0226f65cd5b7c319
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-01 15:33:49 +0200

    Add MainLogger that uses STDERRHandler and FileHandler

--- src/loggers/base.py
+++ src/loggers/base.py
@@ -3,6 +3,20 @@ import logging
 from src.loggers.handlers.stderr import STDERRHandler
 
 
+class MainLogger:
+    def __new__(cls, suppress_stderr=False):
+        from src.loggers.handlers.file import FileHandler
+        from src import LOG_FILE_PATH
+
+        logger = logging.Logger(cls.__name__)
+        file_handler = FileHandler(LOG_FILE_PATH).handler
+        logger.addHandler(file_handler)
+        stdout_handler = STDERRHandler(suppress_stderr).handler
+        logger.addHandler(stdout_handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
 class SimpleLogger:
     def __new__(cls, suppress_stderr=False):
         logger = logging.Logger(cls.__name__)
