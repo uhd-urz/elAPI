11c7fd3782d96f7f3705199def2b15e8b97601c5
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-04 00:06:52 +0200

    Add PathValidator

---
+++ src/validators/path.py
@@ -0,0 +1,65 @@
+import string
+from pathlib import Path
+from random import choices
+from typing import Union, Iterable
+
+from src.loggers import Logger
+from src.path import ProperPath
+from src.validators.base import Validator, ValidationError, COMMON_PATH_ERRORS
+
+logger = Logger()
+
+
+class PathValidator(Validator):
+    def __init__(
+        self,
+        path: Union[Iterable[...], Union[str, ProperPath, Path]],
+        err_logger=logger,
+    ):
+        self.path = path
+        self.err_logger = err_logger
+        self.TMP_FILE = (
+            f".tmp_{''.join(choices(string.ascii_lowercase + string.digits, k=16))}"
+        )
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        if not isinstance(value, (str, ProperPath, Path, Iterable)):
+            raise ValueError(
+                f"{value} must be an instance (or iterable of instances) of str, ProperPath, Path"
+            )
+        try:
+            iter(value)
+        except TypeError:
+            self._path = (value,)
+        else:
+            if isinstance(value, (str, ProperPath, Path)):
+                self._path = (value,)
+            else:
+                self._path = value
+
+    def validate(self):
+        for p in self.path:
+            if not isinstance(p, ProperPath):
+                try:
+                    p = ProperPath(p, err_logger=self.err_logger)
+                except ValueError:
+                    continue
+            if p.expanded:
+                try:
+                    p.create()
+                    with (p / self.TMP_FILE if p.kind == "dir" else p).open(
+                        mode="ba"
+                    ) as f:
+                        f.write(b"\0")
+                        f.truncate(f.tell() - 1)
+                except COMMON_PATH_ERRORS:
+                    continue
+                else:
+                    (p / self.TMP_FILE).remove() if p.kind == "dir" else ...
+                    return p.expanded
+        raise ValidationError(f"Given path(s) could not be validated!")
