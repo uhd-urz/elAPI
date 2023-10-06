fc8c44b6ec8a94614f55fb7e469a5a30cbe4bf16
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-06 13:32:44 +0200

    Remove ProperPath from definitions
    Simplify the fallback logic. Actual path validation is to be done later (log_file.py, config.py)

--- src/_names.py
+++ src/_names.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from typing import Union
 
@@ -16,20 +17,21 @@ cur_dir: Path = Path.cwd()
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
 # XDG and other convention variable definitions
-XDG_DATA_HOME: Union[ProperPath, Path, str] = ProperPath("XDG_DATA_HOME", env_var=True)
-XDG_DOWNLOAD_DIR: Union[ProperPath, Path, str] = ProperPath("XDG_DOWNLOAD_DIR", env_var=True)
-XDG_CONFIG_HOME: Union[ProperPath, Path, str] = ProperPath('XDG_CONFIG_HOME', env_var=True).expanded
+XDG_DATA_HOME: Union[ProperPath, Path, str] = "XDG_DATA_HOME"
+XDG_DOWNLOAD_DIR: Union[ProperPath, Path, str] = "XDG_DOWNLOAD_DIR"
+XDG_CONFIG_HOME: Union[ProperPath, Path, str] = "XDG_CONFIG_HOME"
 TMP_DIR: Path = Path(f"/var/tmp/{APP_NAME}")
 
 # Fallback definitions
 FALLBACK_DIR: Path = user_home / ".local/share" / APP_NAME
-FALLBACK_EXPORT_DIR: Path = user_home / 'Downloads'
-FALLBACK_CONFIG_DIR: Path = user_home / '.config'
+FALLBACK_EXPORT_DIR: Path = user_home / "Downloads"
+FALLBACK_CONFIG_DIR: Path = user_home / ".config"
 
 # Configuration path definitions
 SYSTEM_CONFIG_LOC: Path = Path("/etc") / CONFIG_FILE_NAME
-LOCAL_CONFIG_LOC: Path = XDG_CONFIG_HOME / CONFIG_FILE_NAME if XDG_CONFIG_HOME \
-    else FALLBACK_CONFIG_DIR / CONFIG_FILE_NAME
+LOCAL_CONFIG_LOC: Path = (
+    os.getenv(XDG_CONFIG_HOME) or FALLBACK_CONFIG_DIR
+) / CONFIG_FILE_NAME
 # In case, $XDG_CONFIG_HOME isn't defined in the machine, it falls back to $HOME/.config/elapi.yaml
 PROJECT_CONFIG_LOC: Path = cur_dir / CONFIG_FILE_NAME
 
