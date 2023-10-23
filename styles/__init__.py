5b434c73f09eae9ded380b5a036f4ce88a13e340
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-10-23 11:15:51 +0200

    Import ColorText

--- styles/__init__.py
+++ styles/__init__.py
@@ -2,4 +2,4 @@
 from styles._markdown_doc import get_custom_help_text
 from styles._missing import Missing
 from styles.format import BaseFormat, Format, ValidateLanguage
-from styles.highlight import BaseHighlight, Highlight, NoteText
+from styles.highlight import BaseHighlight, Highlight, NoteText, ColorText
