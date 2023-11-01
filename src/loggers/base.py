09db64dd423fb0339cb505a1b3f75633d3994e04
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2023-11-02 00:57:54 +0100

    Make base logger classes singletons
    Singletons serve our needs better.

--- src/loggers/base.py
+++ src/loggers/base.py
@@ -1,26 +1,33 @@
 import logging
+from typing import Optional
 
 from src.loggers.handlers.stderr import STDERRHandler
 
 
 class MainLogger:
+    logger: Optional[logging.Logger] = None
+
     def __new__(cls, suppress_stderr=False):
         from src.loggers.handlers.file import FileHandler
         from src.configuration.log_file import LOG_FILE_PATH
 
-        logger = logging.Logger(cls.__name__)
-        file_handler = FileHandler(LOG_FILE_PATH).handler
-        logger.addHandler(file_handler)
-        stdout_handler = STDERRHandler(suppress_stderr).handler
-        logger.addHandler(stdout_handler)
-        logger.setLevel(logging.DEBUG)
-        return logger
+        if cls.logger is None:
+            cls.logger = logging.Logger(cls.__name__)
+            file_handler = FileHandler(LOG_FILE_PATH).handler
+            cls.logger.addHandler(file_handler)
+            stdout_handler = STDERRHandler(suppress_stderr).handler
+            cls.logger.addHandler(stdout_handler)
+            cls.logger.setLevel(logging.DEBUG)
+        return cls.logger
 
 
 class SimpleLogger:
+    logger: Optional[logging.Logger] = None
+
     def __new__(cls, suppress_stderr=False):
-        logger = logging.Logger(cls.__name__)
-        stdout_handler = STDERRHandler(suppress_stderr).handler
-        logger.addHandler(stdout_handler)
-        logger.setLevel(logging.DEBUG)
-        return logger
+        if cls.logger is None:
+            cls.logger = logging.Logger(cls.__name__)
+            stdout_handler = STDERRHandler(suppress_stderr).handler
+            cls.logger.addHandler(stdout_handler)
+            cls.logger.setLevel(logging.DEBUG)
+        return cls.logger
