b907f8eb20e26f7bdd7ef9d157bd20ce24f280c1
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-03-31 19:12:18 +0200

    Fix closing of event loop
    If event loop is closed in a finally block, it is also closed for when there is no error (else block is run), and that triggers RuntimeError as well. So we only close the event loop only when an error is encountered while the asynchronous tasks are running.

--- src/elapi/plugins/commons/get_information.py
+++ src/elapi/plugins/commons/get_information.py
@@ -56,18 +56,30 @@ class RecursiveInformation:
         self.endpoint_id_key_name = endpoint_id_key_name
         self.verbose = verbose
 
+    @staticmethod
+    async def close_event_loop(event_loop: asyncio.AbstractEventLoop, endpoint) -> None:
+        await endpoint.close()
+        # Must be closed before cancelling asyncio tasks or stopping the event loop
+        event_loop.set_exception_handler(lambda loop, context: ...)
+        # "lambda loop, context: ..." suppresses asyncio error emission:
+        # https://docs.python.org/3/library/asyncio-dev.html#detect-never-retrieved-exceptions
+        for task in asyncio.all_tasks(event_loop):
+            task.cancel()
+        if event_loop.is_running():
+            event_loop.stop()  # Will raise RuntimeError
+
     async def items(self):
         from ...endpoint import FixedAsyncEndpoint, RecursiveGETEndpoint
         from rich.progress import track
 
         event_loop = asyncio.get_running_loop()
-        _endpoint = FixedAsyncEndpoint(endpoint_name=self.endpoint_name)
+        endpoint = FixedAsyncEndpoint(endpoint_name=self.endpoint_name)
         endpoint_information = Information(self.endpoint_name).items()
         try:
             recursive_endpoint = RecursiveGETEndpoint(
                 endpoint_information,
                 self.endpoint_id_key_name,
-                target_endpoint=_endpoint,
+                target_endpoint=endpoint,
             )
             tasks: list[Awaitable[Response]] = [
                 item for item in recursive_endpoint.endpoints()
@@ -86,6 +98,7 @@ class RecursiveInformation:
                 f"Retrieving {self.endpoint_name} data was interrupted due to a network error. "
                 f"Exception details: '{error!r}'"
             )
+            await self.close_event_loop(event_loop, endpoint)
             raise InterruptedError from error  # This will likely never be reached,
             # since this entire exception block will only trigger while event loop is still running
             # but event loop is already closed in finally block which raises RuntimeError.
@@ -94,17 +107,11 @@ class RecursiveInformation:
                 f"Request for '{self.endpoint_name}' data was received by the server but "
                 f"request was not successful."
             )
+            await self.close_event_loop(event_loop, endpoint)
             raise InterruptedError from e
         except (KeyboardInterrupt, asyncio.CancelledError) as e:
+            await self.close_event_loop(event_loop, endpoint)
             raise Exit(1) from e
         else:
+            await endpoint.close()
             return recursive_information
-        finally:
-            event_loop.set_exception_handler(lambda loop, context: ...)
-            # "lambda loop, context: ..." suppresses asyncio error emission:
-            # https://docs.python.org/3/library/asyncio-dev.html#detect-never-retrieved-exceptions
-            await _endpoint.close()  # Must be closed before cancelling asyncio tasks.
-            for task in asyncio.all_tasks(event_loop):
-                task.cancel()
-            if event_loop.is_running():
-                event_loop.stop()  # Will raise RuntimeError
