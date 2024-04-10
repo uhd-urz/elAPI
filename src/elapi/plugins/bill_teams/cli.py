e307f72498bf1f2ddb98b60282f1f1c527a26fb8
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-04-10 20:27:57 +0200

    Rename argument
    - Assign CLI documentation for argument help.

--- src/elapi/plugins/bill_teams/cli.py
+++ src/elapi/plugins/bill_teams/cli.py
@@ -105,7 +105,7 @@ def get_teams(
 def get_owners(
     owners_data_path: Annotated[
         str,
-        typer.Option("--source-path", help="", show_default=False),
+        typer.Option("--meta-source", help=docs["owners_data_path"], show_default=False),
     ],
     skip_essential_validation: Annotated[
         Optional[bool], typer.Option(hidden=True)
