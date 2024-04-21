c7de82c66a0deea985acff1767341e350bc4c356
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-04-21 18:12:47 +0200

    Add can_overwrite attribute
    - Only overwrite a file if can_overwrite is True or fall back.
    - Show NoteText message about upcoming `--overwrite`.

--- src/elapi/plugins/commons/export.py
+++ src/elapi/plugins/commons/export.py
@@ -76,20 +76,55 @@ class Export:
 
 class ExportValidator(PathValidator):
     def __init__(
-        self, /, export_path: Union[Iterable, Union[None, str, ProperPath, Path]]
+        self,
+        /,
+        export_path: Union[Iterable, Union[None, str, ProperPath, Path]],
+        can_overwrite: bool = False,
     ):
         self.export_path = export_path
+        self.can_overwrite = can_overwrite
         super().__init__(export_path)
+
+    @property
+    def can_overwrite(self) -> bool:
+        return self._can_overwrite
+
+    @can_overwrite.setter
+    def can_overwrite(self, value: bool):
+        if not isinstance(value, bool):
+            raise ValueError("can_overwrite attribute must be a boolean!")
+        self._can_overwrite = value
 
     def validate(self) -> ProperPath:
         from ...configuration import APP_NAME, EXPORT_DIR
+        from ...styles import stdin_console, NoteText
 
         if self.export_path is not None:
             try:
-                return ProperPath(super().validate(), err_logger=logger)
+                path = ProperPath(super().validate(), err_logger=logger)
             except ValidationError:
                 logger.warning(
                     f"--export path '{self.export_path}' couldn't be validated! "
                     f"{APP_NAME} will use fallback export location."
                 )
+            else:
+                if (
+                    path.kind == "file"
+                    and path.expanded.exists()
+                    # Though, unnecessary in most cases as PathValidator already creates the file.
+                    and not self.can_overwrite
+                ):
+                    if path.expanded.read_bytes():
+                        logger.warning(
+                            f"--export path '{self.export_path}' already exists and is not empty! "
+                            f"{APP_NAME} will use fallback export location."
+                        )
+                        stdin_console.print(
+                            NoteText(
+                                "Use '--overwrite' to force '--export' to write to a non-empty file.\n",
+                                stem="Note",
+                            )
+                        )
+                        return ProperPath(EXPORT_DIR, err_logger=logger)
+                return path
         return ProperPath(EXPORT_DIR, err_logger=logger)
