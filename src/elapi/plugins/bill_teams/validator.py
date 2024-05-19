05a75fe2930758523afd7629c3badc2627423aa6
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-05-19 16:35:27 +0200

    Move OwnersDataSpecification to specification.py
    specification.py should now contain all specification related definitions.

--- src/elapi/plugins/bill_teams/validator.py
+++ src/elapi/plugins/bill_teams/validator.py
@@ -1,39 +1,11 @@
-from dataclasses import dataclass
 from typing import Optional, Callable, Union
 
+from .specification import OwnersDataSpecification
 from ...loggers import Logger
 from ...styles import FormatError
 from ...validators import Validator, ValidationError
 
 logger = Logger()
-
-
-# noinspection SpellCheckingInspection
-@dataclass
-class OwnersDataSpecification:
-    TEAM_OWNER_ID: str = "team_owner_id"
-    TEAM_OWNER_FIRST_NAME: str = "team_owner_firstname"
-    TEAM_OWNER_LAST_NAME: str = "team_owner_lastname"
-    TEAM_OWNER_EMAIL: str = "team_owner_email"
-    TEAM_BILLABLE: str = "team_billable"
-    BILLING_UNIT_COST: str = "billing_unitcost"
-    BILLING_MANAGEMENT_FACTOR: str = "billing_managementfactor"
-    BILLING_MANAGEMENT_LIMITL: str = "billing_managementlimit"
-    BILLING_INSTITUTE1: str = "billing_institute1"
-    BILLING_INSTITUTE2: str = "billing_institute2"
-    BILLING_PERSON_GROUP: str = "billing_persongroup"
-    BILLING_STREET: str = "billing_street"
-    BILLING_POSTAL_CODE: str = "billing_postalcode"
-    BILLING_CITY: str = "billing_city"
-    BILLING_INT_EXT: str = "billing_intext"
-    BILLING_ACCOUNT_UNIT: str = "billing_accunit"
-    TEAM_ACRONYM_EXT: str = "team_acronymext"
-    TEAM_ACRONYM_INT: str = "team_acronymint"
-    TEAM_GONE: str = "team_gone"
-    TEAM_SPECIAL: str = "team_special"
-    TEAM_LIME_SURVEY: str = "team_limesurvey"
-    TEAM_SIGNED_CONTRACT: str = "team_signedcontract"
-    TEAM_END_DATE: str = "team_enddate"
 
 
 class SanitizationError(Exception): ...
