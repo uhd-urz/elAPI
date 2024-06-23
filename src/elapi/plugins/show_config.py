b9264d740ae7d609eef5326fecb42121f79a5b90
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-06-23 20:06:33 +0200

    Add information for new configuration fields
    Add configuration information for new configuration fields.
    - Show environment variable as source when it is
    - Use get_active_unsafe_token_warning, get_active_enable_http2, get_active_export_dir, get_active_verify_ssl, get_active_timeout
    - Use minimal_active_configuration instead of history

--- src/elapi/plugins/show_config.py
+++ src/elapi/plugins/show_config.py
@@ -1,29 +1,35 @@
+from .._names import ENV_XDG_DOWNLOAD_DIR
 from ..configuration import (
     APP_NAME,
     inspect,
-    UNSAFE_TOKEN_WARNING,
-    ENABLE_HTTP2,
-    EXPORT_DIR,
+    get_active_unsafe_token_warning,
+    get_active_enable_http2,
+    get_active_export_dir,
+    get_active_verify_ssl,
+    get_active_timeout,
     APP_DATA_DIR,
     TMP_DIR,
     KEY_HOST,
     KEY_API_TOKEN,
     KEY_UNSAFE_TOKEN_WARNING,
     KEY_ENABLE_HTTP2,
+    KEY_VERIFY_SSL,
+    KEY_TIMEOUT,
     KEY_EXPORT_DIR,
     LOG_FILE_PATH,
+    FALLBACK_SOURCE_NAME,
+    minimal_active_configuration,
 )
-
 from ..styles import Missing, ColorText
 from ..styles.colors import RED, BLUE, YELLOW, LIGHTGREEN
 
-detected_config = inspect.applied_config
+detected_config = minimal_active_configuration
 detected_config_files = inspect.applied_config_files
-FALLBACK = "DEFAULT"
 missing = ColorText(Missing())
 
 try:
     api_token_masked = detected_config[KEY_API_TOKEN].value or "''"
+    api_token_masked = "''" if api_token_masked == "" else api_token_masked
     api_token_source = detected_config_files[detected_config[KEY_API_TOKEN].source]
 except KeyError:
     api_token_masked, api_token_source = missing, None
@@ -32,21 +38,41 @@ try:
     unsafe_token_use_source = detected_config[KEY_UNSAFE_TOKEN_WARNING].source
     unsafe_token_use_source = detected_config_files[unsafe_token_use_source]
 except KeyError:
-    unsafe_token_use_source = FALLBACK
+    unsafe_token_use_source = FALLBACK_SOURCE_NAME
 finally:
-    unsafe_token_use_value = "Yes" if UNSAFE_TOKEN_WARNING else "No"
+    unsafe_token_use_value = "Yes" if get_active_unsafe_token_warning() else "No"
 
 try:
     enable_http2_source = detected_config[KEY_ENABLE_HTTP2].source
     enable_http2_source = detected_config_files[enable_http2_source]
 except KeyError:
-    enable_http2_source = FALLBACK
+    enable_http2_source = FALLBACK_SOURCE_NAME
 finally:
-    enable_http2_value = "Yes" if ENABLE_HTTP2 else "No"
+    enable_http2_value = "Yes" if get_active_enable_http2() else "No"
 
 
 try:
-    host_value = detected_config[KEY_HOST].value or "''"
+    verify_ssl_source = detected_config[KEY_VERIFY_SSL].source
+    verify_ssl_source = detected_config_files[verify_ssl_source]
+except KeyError:
+    verify_ssl_source = FALLBACK_SOURCE_NAME
+finally:
+    verify_ssl_value = "Yes" if get_active_verify_ssl() else "No"
+
+
+try:
+    timeout_source = detected_config[KEY_TIMEOUT].source
+    timeout_source = detected_config_files[timeout_source]
+except KeyError:
+    timeout_source = FALLBACK_SOURCE_NAME
+finally:
+    timeout_value = get_active_timeout()
+    timeout_value = f"{timeout_value} " + ("seconds" if timeout_value > 1 else "second")
+
+
+try:
+    host_value = detected_config[KEY_HOST].value
+    host_value = "''" if host_value == "" else host_value
     host_source = detected_config_files[detected_config[KEY_HOST].source]
 except KeyError:
     host_value, host_source = missing, None
@@ -54,7 +80,7 @@ except KeyError:
 try:
     export_dir_source = detected_config_files[detected_config[KEY_EXPORT_DIR].source]
 except KeyError:
-    export_dir_source = FALLBACK
+    export_dir_source = detected_config[KEY_EXPORT_DIR].source
 
 
 detected_config_files_formatted = "\n- " + "\n- ".join(
@@ -107,7 +133,7 @@ The following debug information includes configuration values and their sources 
             if not no_keys
             else ""
         )
-        + f": {EXPORT_DIR} ← `{export_dir_source}`"
+        + f": {get_active_export_dir()} ← `{export_dir_source}`"
         + f"""
 - {ColorText('App data directory').colorize(LIGHTGREEN)}: {APP_DATA_DIR}
 - {ColorText('Caching directory').colorize(LIGHTGREEN)}: {TMP_DIR}
@@ -128,6 +154,22 @@ The following debug information includes configuration values and their sources 
             else ""
         )
         + f": {enable_http2_value} ← `{enable_http2_source}`"
+        + "\n"
+        + f"- {ColorText('Verify SSL certificate').colorize(LIGHTGREEN)}"
+        + (
+            f" **[{ColorText(KEY_VERIFY_SSL.lower()).colorize(YELLOW)}]**"
+            if not no_keys
+            else ""
+        )
+        + f": {verify_ssl_value} ← `{verify_ssl_source}`"
+        + "\n"
+        + f"- {ColorText('Timeout duration').colorize(LIGHTGREEN)}"
+        + (
+            f" **[{ColorText(KEY_TIMEOUT.lower()).colorize(YELLOW)}]**"
+            if not no_keys
+            else ""
+        )
+        + f": {timeout_value} ← `{timeout_source}`"
         + f"""
 
 
@@ -136,10 +178,22 @@ The following debug information includes configuration values and their sources 
 """
         + (
             f"""
-- `{FALLBACK}`: Fallback value for when no user configuration is found.
+- `{FALLBACK_SOURCE_NAME}`: Fallback value for when no user configuration is found.
 """
-            if FALLBACK
-            in (export_dir_source, unsafe_token_use_source, enable_http2_source)
+            if FALLBACK_SOURCE_NAME
+            in (
+                export_dir_source,
+                unsafe_token_use_source,
+                enable_http2_source,
+                verify_ssl_source,
+            )
+            else ""
+        )
+        + (
+            f"""
+- `{ENV_XDG_DOWNLOAD_DIR}`: Environment variable.
+"""
+            if ENV_XDG_DOWNLOAD_DIR in (export_dir_source,)
             else ""
         )
     )
