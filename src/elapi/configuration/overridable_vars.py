6e42a9c1600f8a007c9bd64d885a0aeb550a4d76
Mahadi Xion <mahadi.xion@urz.uni-heidelberg.de>
2024-08-10 16:14:59 +0200

    Add get_active_host_url_without_api_suffix
    Sometimes we want to work with the actual URL that targets the main browser interface instead the API interface.

--- src/elapi/configuration/overridable_vars.py
+++ src/elapi/configuration/overridable_vars.py
@@ -9,6 +9,12 @@ def get_active_host() -> str:
     return MinimalActiveConfiguration().get_value(KEY_HOST)
 
 
+def get_active_host_url_without_api_suffix() -> str:
+    import re
+    from .config import ELAB_HOST_URL_API_SUFFIX
+    return re.sub(ELAB_HOST_URL_API_SUFFIX, r"", get_active_host(), count=1, flags=re.IGNORECASE)
+
+
 def get_active_api_token() -> APIToken:
     from .config import KEY_API_TOKEN, MinimalActiveConfiguration
 
